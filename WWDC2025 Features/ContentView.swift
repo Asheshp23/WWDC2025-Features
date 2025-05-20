0798ddb103f102f0b711347369af7cbf3150338c
asheshp23 <asheshp23@gmail.com>
2025-05-20 11:01:48 -0400

    Initial Commit

---
+++ WWDC2025 Features/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WWDC2025 Features
+//
+//  Created by Ashesh Patel on 2025-05-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WWDC2025 Features/WWDC2025_FeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC2025_FeaturesApp.swift
+//  WWDC2025 Features
+//
+//  Created by Ashesh Patel on 2025-05-20.
+//
+
+import SwiftUI
+
+@main
+struct WWDC2025_FeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
